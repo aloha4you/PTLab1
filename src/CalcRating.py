091b37853ede22f8be7eccdd2d493257ab7dc363
Sav Desktop <maksim0211@gmail.com>
2022-11-12 17:55:35 +0300

    update CalcRating.py for calculating `good` students

--- src/CalcRating.py
+++ src/CalcRating.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 from Types import DataType
 
-RatingType = dict[str, float]
-
+RatingType = dict[str, bool]
 
 class CalcRating:
 
     def __init__(self, data: DataType) -> None:
         self.data: DataType = data
-        self.rating: RatingType = {}
+        self.tmp_rating: RatingType = {}
+        self.res_names: list[str] = []
 
-    def calc(self) -> RatingType:
+    # Возвращает список хорошистов и их количество
+    def calc(self) -> (list[str], int):
+        # Временный словарь, Имя студента -> Является ли хорошистом
         for key in self.data:
-            self.rating[key] = 0.0
-            for subject in self.data[key]:
-                self.rating[key] += subject[1]
-            self.rating[key] /= len(self.data[key])
-        return self.rating
+            self.tmp_rating[key] = True
+            for (subject_name, subject_score) in self.data[key]:
+                self.tmp_rating[key] = self.tmp_rating[key] and subject_score >= 76
+
+        # Результирующий словарь, оставляющий только студентов хорошистов (я не умею в лямбды на питоне)
+        for (key, value) in self.tmp_rating.items():
+            if value:
+                self.res_names.append(key)
+        return self.res_names, len(self.res_names)
