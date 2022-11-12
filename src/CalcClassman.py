8bbdf9ba3b66dbf9f02eddc19debd629ada1d551
Sav Desktop <maksim0211@gmail.com>
2022-11-12 18:12:40 +0300

    make own class for calculating classmans

---
+++ src/CalcClassman.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from Types import DataType
+
+ClassmanType = dict[str, bool]
+
+class CalcClassman:
+
+    def __init__(self, data: DataType) -> None:
+        self.data: DataType = data
+        self.tmp_rating: ClassmanType = {}
+        self.res_names: list[str] = []
+
+    # Возвращает список хорошистов и их количество
+    def calc(self) -> (list[str], int):
+        # Временный словарь, Имя студента -> Является ли хорошистом
+        for key in self.data:
+            self.tmp_rating[key] = True
+            for (subject_name, subject_score) in self.data[key]:
+                self.tmp_rating[key] = self.tmp_rating[key] and subject_score >= 76
+
+        # Результирующий словарь, оставляющий только студентов хорошистов (я не умею в лямбды на питоне)
+        for (key, value) in self.tmp_rating.items():
+            if value:
+                self.res_names.append(key)
+        return self.res_names, len(self.res_names)
